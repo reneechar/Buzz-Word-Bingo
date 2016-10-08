7e746f0ac7974354306942a94d824a21863e16ed
Renee <rchar20@gmail.com>
2016-10-08 09:10:57 -1000

    moved scores.js outside of routes folder

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const scores = require('./scores.js');
+const scores = require('../scores.js');
 
 
 router.get('/buzzwords', (req,res) => {
